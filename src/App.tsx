ff631896307b4b488a7ef99cd09930a6a5531a4a
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-05-28 19:19:54 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { T
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toasx />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+  <QueryClientProv
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
