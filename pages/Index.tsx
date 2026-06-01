620cbc15739f13082c572117f89767eedf4c3e91
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-01 05:55:33 +0530

    Update Index.tsx

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -8,14 +8,7 @@ import ShutterSplash from '@/components/ShutterSplash';
 import MarqueeStrip from '@/compone
     <main className="relative min-h-screen overflow-x-hidden w-full">
       {/* Shutter Splash */}
-      {!splashDone && <ShutterSplash onComplete={handleSplashComplete} />}
-
-      {/* Animated Background */}
-      <AnimatedBackground />
-      
-      {/* Navigation */}
-      <Navbar />
-      
+      {!splashDon
       {/* Main Content */}
       <Hero />
       <MarqueeStrip />
