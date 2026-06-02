6cff879d366c948513b921c43384233b813bc7b3
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-02 23:54:24 +0530

    Update AnimatedBackground.tsx

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -6,10 +6,7 @@ export default function AnimatedBackground() {
   return (
     <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none bg-background">
       {/* orbs (dName="absolute -top-32 -left-32 w-[40rem] h-[40rem] rounded-full bg-primary/20 blur-3xl animate-blob" />
-          <div clte for hero readability */}
-      <div className="absolute inset-x-0 top-0 h-72 bg-gradient-to-b from-background/60 to-transparent" />
-      {/* Bottom vignette */}
-      <div className="absolute inset-x-0 bottom-0 h-72 bg-gradient-to-t from-background/60 to-transparent" />
+          lassName="absolute inset-x-0 bottom-0 h-72 bg-gradient-to-t from-background/60 to-transparent" />
     </div>
   );
 }
