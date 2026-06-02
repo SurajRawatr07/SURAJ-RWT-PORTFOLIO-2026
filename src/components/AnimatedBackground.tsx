3e911ba00c82a375135fe60dcbfb73dc636b086c
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-02 23:54:13 +0530

    Update AnimatedBackground.tsx

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -8,12 +8,7 @@ export default function AnimatedBackground() {
       {/* Soft mesh g
         className="absolute inse
       />ent orbs (dName="absolute -top-32 -left-32 w-[40rem] h-[40rem] rounded-full bg-primary/20 blur-3xl animate-blob" />
-          <div className="absolute top-1/3 -right-40 w-[36rem] h-[36rem] rounded-full bg-accent/20 blur-3xl animate-blob animation-delay-2000" />
-          <div className="absolute bottom-0 left-1/3 w-[34rem] h-[34rem] rounded-full bg-primary/15 blur-3xl animate-blob animation-delay-4000" />
-        </>
-      )}
-
-      {/* Top vignette for hero readability */}
+          <div clte for hero readability */}
       <div className="absolute inset-x-0 top-0 h-72 bg-gradient-to-b from-background/60 to-transparent" />
       {/* Bottom vignette */}
       <div className="absolute inset-x-0 bottom-0 h-72 bg-gradient-to-t from-background/60 to-transparent" />
