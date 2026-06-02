26d42586a22c97161d99b2aa2efcd93f6fa667b9
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-02 23:54:07 +0530

    Update AnimatedBackground.tsx

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -7,10 +7,7 @@ export default function AnimatedBackground() {
     <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none bg-background">
       {/* Soft mesh g
         className="absolute inse
-      />ent orbs (disabled on mobile for perf) */}
-      {!isMobile && (
-        <>
-          <div className="absolute -top-32 -left-32 w-[40rem] h-[40rem] rounded-full bg-primary/20 blur-3xl animate-blob" />
+      />ent orbs (dName="absolute -top-32 -left-32 w-[40rem] h-[40rem] rounded-full bg-primary/20 blur-3xl animate-blob" />
           <div className="absolute top-1/3 -right-40 w-[36rem] h-[36rem] rounded-full bg-accent/20 blur-3xl animate-blob animation-delay-2000" />
           <div className="absolute bottom-0 left-1/3 w-[34rem] h-[34rem] rounded-full bg-primary/15 blur-3xl animate-blob animation-delay-4000" />
         </>
