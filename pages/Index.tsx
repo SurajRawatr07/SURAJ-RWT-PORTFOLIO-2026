59bbf90d67c401f5e42164a6e93d2248025a152d
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-04 22:08:13 +0530

    Update Index.tsx

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -1,10 +1,6 @@
 import { useState, useCallback } from 'react';
 import Navbar from '@/components/Navbar';
-import Hero from '@/components/Hero';
-import About from '@/components/About';
-import Experience from '@/components/Experience';
-importaimatedBackground />
-      
+impor
       {/* Navigation */}
       <Navbar />
       
