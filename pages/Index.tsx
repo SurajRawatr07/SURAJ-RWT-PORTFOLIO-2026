8ab6e63e8d276b6c64fb00716a783ba3dde70e06
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-04 22:08:08 +0530

    Update Index.tsx

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -3,12 +3,7 @@ import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Experience from '@/components/Experience';
-importashDone, setSplashDone] = useState(false);
-  const handter Splash */}
-      {!splashDone && <ShutterSplash onComplete={handleSplashComplete} />}
-
-      {/* Animated Background */}
-      <AnimatedBackground />
+importaimatedBackground />
       
       {/* Navigation */}
       <Navbar />
