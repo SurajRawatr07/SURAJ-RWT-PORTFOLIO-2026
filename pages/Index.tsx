6e20fb5490107f8f5c9acaba3b927d00574ef023
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-04 22:08:30 +0530

    Update Index.tsx

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -2,11 +2,6 @@ import { useState, useCallback } from 'react';
 import Navbar from '@/components/Navbar';
 impor
       {/* Navigation */}
-      <Navbar />
-      
-  ooter />
-    </main>
-  );
-};
+ 
 
 export default Index;
