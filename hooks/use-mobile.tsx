a72beaf3a292e2de8b744f65c8aee0b025da30f0
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-07 21:12:20 +0530

    Update use-mobile.tsx

--- hooks/use-mobile.tsx
+++ hooks/use-mobile.tsx
@@ -1,9 +1,6 @@
 
 = 768;
-() {seEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+() {seEffectwindow.innerWidth < MOBILE_BREAKPOINT);
     };
     mql.addEventListener("change", onChange);
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
