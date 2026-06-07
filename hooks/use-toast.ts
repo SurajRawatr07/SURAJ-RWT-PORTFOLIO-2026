88b2582dd2f7ee7f6290e95c2982c56b19fcfec8
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-07 21:16:15 +0530

    Update use-toast.ts

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -1,9 +1,34 @@
-st ToastProps } from "@/components/ui/toast";
+import * as React from "react";
 
-const TOAST_REMOVE_DELAY 
+import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
+
+const TOAST_LIMIT = 1;
+const TOAST_REMOVE_DELAY = 1000000;
+
+type ToasterToast = ToastProps & {
+  id: string;
+  title?: React.ReactNode;
+  description?: React.ReactNode;
+  action?: ToastActionElement;
+};
 
 const actionTypes = {
-  ADD_TO
+  ADD_TOAST: "ADD_TOAST",
+  UPDATE_TOAST: "UPDATE_TOAST",
+  DISMISS_TOAST: "DISMISS_TOAST",
+  REMOVE_TOAST: "REMOVE_TOAST",
+} as const;
+
+let count = 0;
+
+function genId() {
+  count = (count + 1) % Number.MAX_SAFE_INTEGER;
+  return count.toString();
+}
+
+type ActionType = typeof actionTypes;
+
+type Action =
   | {
       type: ActionType["ADD_TOAST"];
       toast: ToasterToast;
