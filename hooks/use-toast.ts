678b974653e5acbe4c457f54441ad57a4a90c47a
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-07 21:14:30 +0530

    Update use-toast.ts

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -1,5 +1,4 @@
-import * as Rea
-import type { ToastActionElement, ToastProps } from "@/components/ui/toast";
+stActionElement, ToastProps } from "@/components/ui/toast";
 
 const TOAST_LIMIT = 1;
 const TOAST_REMOVE_DELAY = 1000000;
