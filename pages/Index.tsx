fe75fb08681ba675fd5752a92a071a9de5bc4c19
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-07 21:07:40 +0530

    Update Index.tsx

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -2,13 +2,7 @@ import { useState, useCallback } from 'react';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
-import Experience from '@/components/Experience';
-import Skills from '@/components/Skills';
-import Projects from '@/cponents/MarqueeStrip';
-
-const Index = me="relative min-h-screen overflow-x-hidden w-full">
-      {/* Shutter Splash */}
-      {!splashDone && <ShutterSplash onComplete={handleSplashComplete} />}
+impashDone && <ShutterSplash onComplete={handleSplashComplete} />}
 
       {/* Animated Background */}
       <AnimatedBackground />
