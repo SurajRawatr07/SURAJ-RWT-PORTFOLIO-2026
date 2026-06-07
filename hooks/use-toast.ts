5a76cd22f21d46fbdedf980d79210cafb6d0f5ad
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-07 21:14:47 +0530

    Update use-toast.ts

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -2,7 +2,7 @@ st ToastProps } from "@/components/ui/toast";
 
 const TOAST_REMOVE_DELAY = 1000000;
 
-type ToasterToast = ToastProps & {
+type ToasterToast = Toops & {
   id: string;
   title?: React.ReactNode;
   description?: React.ReactNode;
