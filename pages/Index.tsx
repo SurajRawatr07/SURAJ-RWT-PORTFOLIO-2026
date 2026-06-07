d986350366f6657996cb34996ecbf8501ff44580
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-07 21:07:48 +0530

    Update Index.tsx

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -4,12 +4,6 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 impashDone && <ShutterSplash onComplete={handleSplashComplete} />}
 
-      {/* Animated Background */}
-      <AnimatedBackground />
-      
-      {/* Navigation */}
-      <Navbar />
-      
       {/* Main Content */}
       <Hero />
       <MarqueeStrip />
