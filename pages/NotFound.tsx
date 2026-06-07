0a8cf6453e4d15a575db8891bee9ffca9239078e
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-07 21:10:39 +0530

    Update NotFound.tsx

--- pages/NotFound.tsx
+++ pages/NotFound.tsx
@@ -2,10 +2,7 @@ import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
 const NotFound = () => {
-     <h1 className="mb-4 text-4xl font-bold">404</h1>
-        <p ref="/" className="text-primary underline hover:text-primary/90">
-          Re
-      </div>
+    v>
  
 
 
