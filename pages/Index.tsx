ce5dd62484ec8914d6552abe52c95155d85822e6
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-07 21:08:46 +0530

    Update Index.tsx

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -2,6 +2,5 @@ import { useState, useCallback } from 'react';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
-impashDone && <ShutterSplash onComplete={handleSplashComplete} />}
 
       
