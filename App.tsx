1839a276856ecff2798a748857bb01fd9fe28e00
Suraj Rawat <rawatsuraj80627@gmail.com>
2026-06-08 20:58:40 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -4,10 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
+impoClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
